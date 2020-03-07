cdf3d7c4ce2b9d03acd03d72775b045079e71e54
gouzal <larbi-inco@live.fr>
2020-03-07 21:50:40 +0100

    comment customize methods

--- src/main/java/com/gouzal/notebook/models/Command.java
+++ src/main/java/com/gouzal/notebook/models/Command.java
@@ -12,6 +12,15 @@ public class Command {
 
     String code;
 
+    /**
+     * This method split the user script and check if it is valid and if the langague is supported,
+     * after taht return the language interpreter name. otherwise rise an exception
+     * @return the Interpreter that used by the User
+     * @throws UnsupportedLanguageException if the Interpreter is not supported by the application
+     * @throws InvalidScriptException if the script format is invalid
+     * @see InvalidScriptException
+     * @See UnsupportedLanguageException
+     */
     public String getInterpreter() throws UnsupportedLanguageException, InvalidScriptException {
         this.getScript();
         String interpreterWithPrefix =code.split(" ")[0];
@@ -21,6 +30,13 @@ public class Command {
         }
         throw new UnsupportedLanguageException(interpreter);
     }
+
+    /**
+     * check whether the code send by the user is valid or not.
+     * for the moment it just check if it starts with % or not.
+     * @return the second part of the code that contain the script to be executed
+     * @throws InvalidScriptException if the the script is invalid
+     */
     public String getScript() throws InvalidScriptException {
         if(this.code.contains("%")){
             return this.code.split(" ",2)[1];
