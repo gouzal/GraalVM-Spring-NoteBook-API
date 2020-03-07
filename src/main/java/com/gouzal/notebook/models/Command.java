e216101651d53b56815de7c06d46421520aececd
gouzal <larbi-inco@live.fr>
2020-03-07 20:33:05 +0100

    add a pojo for command handling

---
+++ src/main/java/com/gouzal/notebook/models/Command.java
@@ -0,0 +1,10 @@
+package com.gouzal.notebook.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Command {
+    String code;
+}
--- src/main/java/com/gouzal/notebook/rest/ExecutorController.java
+++ src/main/java/com/gouzal/notebook/rest/ExecutorController.java
@@ -1,16 +1,17 @@
 package com.gouzal.notebook.rest;
 
+import com.gouzal.notebook.models.Command;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExecutorController {
 
     @GetMapping(value = "/execute", produces = MediaType.APPLICATION_JSON_VALUE)
-    public String execute() {
-        int i=1;
-        return "result:"+i;
+    public String execute(@RequestBody Command command) {
+        return "result:"+command.getCode();
     }
 
 }
