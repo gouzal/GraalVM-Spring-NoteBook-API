bf6c1e7cb2bd7f461153240ff49c52c8115f6838
gouzal <larbi-inco@live.fr>
2020-03-07 16:34:37 +0100

    initial commit

---
+++ src/test/java/com/gouzal/notebook/NotebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gouzal.notebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotebookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
