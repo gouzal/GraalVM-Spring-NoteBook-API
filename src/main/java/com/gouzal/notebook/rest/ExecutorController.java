2cf8a30d455d5b80f1e06ebe09df0cfd1d83dbeb
gouzal <larbi-inco@live.fr>
2020-03-07 20:38:55 +0100

    simple graalVM/JS code testing

--- src/main/java/com/gouzal/notebook/rest/ExecutorController.java
+++ src/main/java/com/gouzal/notebook/rest/ExecutorController.java
@@ -1,22 +1,24 @@
 package com.gouzal.notebook.rest;
 
 import com.gouzal.notebook.models.Command;
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import sun.awt.SunHints;
 
-import javax.naming.Context;
 
 @RestController
 public class ExecutorController {
 
     @GetMapping(value = "/execute", produces = MediaType.APPLICATION_JSON_VALUE)
     public String execute(@RequestBody Command command) {
+        Context context = Context.create();
+        Value result = context.eval("js", "42");
+        context.close();
 
-
-        return "result:"+command.getCode();
+        return "result:" + result.asInt();
     }
 
 }
