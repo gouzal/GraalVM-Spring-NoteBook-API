c456da4ee8fbcd840cbcdc182ccf8db9e822a7eb
gouzal <larbi-inco@live.fr>
2020-03-07 20:52:39 +0100

    return result as JSON

--- src/main/java/com/gouzal/notebook/rest/ExecutorController.java
+++ src/main/java/com/gouzal/notebook/rest/ExecutorController.java
@@ -3,22 +3,28 @@ package com.gouzal.notebook.rest;
 import com.gouzal.notebook.models.Command;
 import org.graalvm.polyglot.Context;
 import org.graalvm.polyglot.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 @RestController
 public class ExecutorController {
 
     @GetMapping(value = "/execute", produces = MediaType.APPLICATION_JSON_VALUE)
-    public String execute(@RequestBody Command command) {
+    public ResponseEntity execute(@RequestBody Command command) {
         Context context = Context.create();
         Value result = context.eval(command.getInterpreter(), command.getScript());
+        Map resultResponse = new HashMap<String, String>();
+        resultResponse.put("result", result.toString());
         context.close();
-
-        return "result:" + result.asInt();
+        return new ResponseEntity<Map>(resultResponse, HttpStatus.OK);
     }
 
 }
