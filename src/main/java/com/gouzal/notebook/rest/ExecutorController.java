5482bb8b96337e12f259ccadfb583d9e3bf3b3da
gouzal <larbi-inco@live.fr>
2020-03-07 20:35:33 +0100

    add GraalVM dependencies

--- src/main/java/com/gouzal/notebook/rest/ExecutorController.java
+++ src/main/java/com/gouzal/notebook/rest/ExecutorController.java
@@ -5,12 +5,17 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import sun.awt.SunHints;
+
+import javax.naming.Context;
 
 @RestController
 public class ExecutorController {
 
     @GetMapping(value = "/execute", produces = MediaType.APPLICATION_JSON_VALUE)
     public String execute(@RequestBody Command command) {
+
+
         return "result:"+command.getCode();
     }
 
