c1c5f008bfefea5b2a2b0db26de795fc258f3c9d
gouzal <larbi-inco@live.fr>
2020-03-08 18:01:59 +0100

    add Test for Util calss

---
+++ src/test/java/com/gouzal/notebook/common/UtilTest.java
@@ -0,0 +1,27 @@
+package com.gouzal.notebook.common;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class UtilTest {
+    @DisplayName("Test Filter utility method")
+    @Test
+    void testFilter() {
+        List<String> words = Arrays.asList("2", "undefined", "hello", "world", "null", "Spring", "8");
+        List<String> newList=new ArrayList<>();
+        for (String word:words){
+            if(word.equalsIgnoreCase("undefined") || word.equalsIgnoreCase(null)){
+                assertEquals("",Util.filter(word));
+            }
+        }
+    }
+
+}
